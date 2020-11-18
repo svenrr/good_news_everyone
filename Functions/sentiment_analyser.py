57337fdf4efcb6ab87aabd29fcd09f607f917eef
eraser070 <62172283+eraser070@users.noreply.github.com>
2020-11-18 15:31:39 +0100

    Add files via upload

---
+++ Functions/sentiment_analyser.py
@@ -0,0 +1,34 @@
+import pickle
+import os
+from text_lemmatization import Lemmatizer
+
+class Sentiment_Analyser():
+    def __init__(self):
+
+        # get total path, in case function is called from somewhere else
+        total_path = os.path.dirname(os.path.realpath(__file__)) + '/'
+
+        self.tfidf = pickle.load(open(total_path + 'tfidf_sentiment.pkl', 'rb'))
+        self.lemmatizer = Lemmatizer()
+        self.vc = pickle.load(open(total_path + 'vc_sentiment.pkl', 'rb'))
+    def preprocess(self, X):
+        
+        # Check if X is string, turn to list
+        if type(X) == str:
+            X = [X]
+                    
+        # Lemmatization
+        X_lem = [self.lemmatizer.lem_text(x) for x in X]
+                
+        # Tfidf vectorization
+        X_tfidf = self.tfidf.transform(X_lem)
+        
+        return X_tfidf
+    
+    def pred(self, X):
+        
+        # preprocess
+        X_tfidf = self.preprocess(X)
+        
+        # return categories
+        return self.vc.predict(X_tfidf)
