f5a8196c2619ef4b98c5e1099006dbb2f503d6f0
Fabian Maerz <fabimaerz@googlemail.com>
2020-11-18 15:17:01 +0100

    create categorizer in functions

---
+++ Functions/categorizer.py
@@ -0,0 +1,33 @@
+import pickle
+import os
+
+class Categorizer():
+    def __init__(self):
+
+        # get total path, in case function is called from somewhere else
+        total_path = os.path.dirname(os.path.realpath(__file__)) + '/'
+
+        self.tfidf = pickle.load(open(total_path + 'tfidf_categorizer.pkl', 'rb'))
+        #self.lemmatizer = lemmatizer
+        self.svc = pickle.load(open(total_path + 'svc_categorizer.pkl', 'rb'))
+    def preprocess(self, X):
+        
+        # Check if X is string, turn to list
+        if type(X) == str:
+            X = [X]
+                    
+        # Lemmatization
+        #X_lem = [self.lemmatizer.lem_text(x) for x in X]
+                
+        # Tfidf vectorization
+        X_tfidf = self.tfidf.transform(X)
+        
+        return X_tfidf
+    
+    def pred(self, X):
+        
+        # preprocess
+        X_tfidf = self.preprocess(X)
+        
+        # return categories
+        return self.svc.predict(X_tfidf)
